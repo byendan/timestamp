2c5bdae76522f70846b2faf1d12bdfe898bc7ab3
Brendan <byendanb@gmail.com>
2016-12-06 09:17:13 +0000

    accepts any date type and gives back timestamp and human readable date. complete

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express')
 var logger = require("morgan");
+var monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+]
 
 var app = express()
 app.use(logger())
@@ -7,16 +10,33 @@ app.use(logger())
 app.get('/:date', function (req, res) {
   
   var inputDate = new Date(req.params.date)
+  if (inputDate == "Invalid Date") {
+      inputDate = new Date(req.params.date * 1000)
+   
+  }
+ 
   if ( inputDate != "Invalid Date" ) {
-      console.log(inputDate)
+  
+       if (+inputDate == inputDate) {
+            res.end("Plus input same as input")
+        } else {
+            var readString = ""
+            readString += monthNames[inputDate.getMonth()] + " " + inputDate.getDate()+ ", " + inputDate.getFullYear().toString()
+           
+            var readableReturn = {
+                "unix": inputDate / 1000,
+                "natural": readString
+            }
+            res.end(JSON.stringify(readableReturn))
+        }
     
-      res.end("It could be: " + inputDate)
+     // res.end("It could be: " + req.params.date)
   } else {
       var nullDate = {
           "unix": null,
           "natural": null
       }
-      console.log(JSON.stringify(nullDate))
+ 
       
       res.end(JSON.stringify(nullDate))
   }
