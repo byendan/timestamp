7e777e45aff687528f63795f8d2540a688753d86
Brendan <byendanb@gmail.com>
2016-12-06 08:28:56 +0000

    Method for invalid date complete

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 var express = require('express')
+var logger = require("morgan");
 
 var app = express()
+app.use(logger())
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/:date', function (req, res) {
+  
+  var inputDate = new Date(req.params.date)
+  if ( inputDate != "Invalid Date" ) {
+      console.log(inputDate)
+    
+      res.end("It could be: " + inputDate)
+  } else {
+      var nullDate = {
+          "unix": null,
+          "natural": null
+      }
+      console.log(JSON.stringify(nullDate))
+      
+      res.end(JSON.stringify(nullDate))
+  }
 })
 
 app.listen(8080, function () {
